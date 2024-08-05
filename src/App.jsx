2427ea6396d6458e9251a4f430c418ad6e63bce3
= <rafiromadhon006@gmail.com>
2024-08-05 11:59:40 +0700

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+import { useState } from 'react'
+
+function Square(){
+  return <button className='square'>X</button>
+}
+
+function App() {
+
+  return (
+    <>
+    <Square/>
+    <Square/>
+    <Square/>
+    <Square/>
+    <Square/>
+    <Square/>
+    <Square/>
+    <Square/>
+    <Square/>
+    </>
+  )
+}
+
+export default App
