8c85869e25210b18d955df8e1a8c65c3860b6f6c
= <rafiromadhon006@gmail.com>
2024-08-05 13:28:46 +0700

    Adding logic to set winner of the gamae

--- src/App.jsx
+++ src/App.jsx
@@ -11,14 +11,30 @@ function Square({ value, onSquareClick }){
     
     export default function Board() {
         const [squares, setSquares] = useState(Array(9).fill(null));
+        const [xIsNext, setXIsNext] = useState(true);
 
         function handleClick(i) {
+            if (squares[i] || calculateWinner(squares)) return;
+
             const nextSquares = squares.slice();
-            nextSquares[i] = 'X'; 
+
+            nextSquares[i] = (xIsNext) ? 'X' : 'O' ;
+            // nextSquares[i] = 'X'; 
             setSquares(nextSquares);
+            setXIsNext(!xIsNext);
+        }
+
+        const winner = calculateWinner(squares);
+        let status = '';
+        if (winner) {
+            status = 'Winner: ' + winner;
+        } else{
+            status = 'Next player: ' + (xIsNext ? 'X' : 'O');
         }
 
         return (
+            <>
+            <div className='status'>{status}</div>
             <div className='board'>
             <Square value={squares[0]} onSquareClick={() => handleClick(0)}/>
             <Square value={squares[1]} onSquareClick={() => handleClick(1)}/>
@@ -30,6 +46,28 @@ function Square({ value, onSquareClick }){
             <Square value={squares[7]} onSquareClick={() => handleClick(7)}/>
             <Square value={squares[8]} onSquareClick={() => handleClick(8)}/>
             </div>
+            </>
             )
         }
         
+        function calculateWinner(squares) {
+            const lines = [
+                [0,1,2],
+                [3,4,5],
+                [6,7,8],
+                [0,3,6],
+                [1,4,7],
+                [2,5,8],
+                [0,4,8],
+                [2,4,6],
+            ];
+
+            for (let i = 0; i < lines.length; i++) {
+                const [a,b,c] = lines[i];
+
+                if (squares[a] === squares[b] && squares[b] === squares[c]) {
+                    return squares[a];
+                }
+            }
+            return false;
+        }
