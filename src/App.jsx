35e2d92e6c0e1bad7ac1384d37d49195eed6abaa
= <rafiromadhon006@gmail.com>
2024-08-05 12:54:04 +0700

    edit app.jss to show x when box have clicked

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,35 @@
 import { useState } from 'react'
 
-function Square(){
-  return <button className='square'>X</button>
-}
+function Square({ value, onSquareClick }){
+    
+    return (
+        <button className='square' onClick={onSquareClick}>
+        {value}
+        </button>
+        );
+    }
+    
+    export default function Board() {
+        const [squares, setSquares] = useState(Array(9).fill(null));
 
-function App() {
+        function handleClick(i) {
+            const nextSquares = squares.slice();
+            nextSquares[i] = 'X'; 
+            setSquares(nextSquares);
+        }
 
-  return (
-    <>
-    <Square/>
-    <Square/>
-    <Square/>
-    <Square/>
-    <Square/>
-    <Square/>
-    <Square/>
-    <Square/>
-    <Square/>
-    </>
-  )
-}
-
-export default App
+        return (
+            <div className='board'>
+            <Square value={squares[0]} onSquareClick={() => handleClick(0)}/>
+            <Square value={squares[1]} onSquareClick={() => handleClick(1)}/>
+            <Square value={squares[2]} onSquareClick={() => handleClick(2)}/>
+            <Square value={squares[3]} onSquareClick={() => handleClick(3)}/>
+            <Square value={squares[4]} onSquareClick={() => handleClick(4)}/>
+            <Square value={squares[5]} onSquareClick={() => handleClick(5)}/>
+            <Square value={squares[6]} onSquareClick={() => handleClick(6)}/>
+            <Square value={squares[7]} onSquareClick={() => handleClick(7)}/>
+            <Square value={squares[8]} onSquareClick={() => handleClick(8)}/>
+            </div>
+            )
+        }
+        
